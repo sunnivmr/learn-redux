4097232e4fe1bfd165bae052a31fceff01736b09
sunnivmr <sunnivmr@stud.ntnu.no>
2020-07-13 16:20:24 +0200

    Created first reducer

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const BUY_CAKE = "BUY_CAKE";
 function buyCake() {
   return {
     type: BUY_CAKE,
-    info: "My first redux action, pls let me commit",
+    info: "Cake has been bought",
   };
 }
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+// If action is BUY_CAKE, the number of cakes decreases by 1
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state, // Make a copy of the state onject and then change only the parts that need to be changed
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
