a708e9a4e97c33243aa40e3bab439184dc67b441
sunnivmr <sunnivmr@stud.ntnu.no>
2020-07-13 17:07:43 +0200

    Added new reducer for icecreams and combined it with cakereducer to create one store

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
-// Action
+// Action to buy cake
 function buyCake() {
   return {
     type: BUY_CAKE,
-    info: "Cake has been bought",
+    info: " 1 Cake has been bought",
   };
 }
 
-// Initial state
-const initialState = {
+// Action to buy ice cream
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "1 Icecream has been bought",
+  };
+}
+
+// Initial cake state
+const initialCakeState = {
   numOfCakes: 10,
 };
 
-// Reducer
+// Initial icecream state
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+
+// Reducers
 // Controls how transitions happen
+
+// Cake reducer
 // If action is BUY_CAKE, the number of cakes decreases by 1
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -32,8 +49,29 @@ const reducer = (state = initialState, action) => {
   }
 };
 
+// Icecream reducer
+// If action is BUY_ICECREAM, the number of icecreams descreases by 1
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
 // Creating store
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 console.log("Initial state", store.getState());
 
 // Adds listener to store
@@ -44,4 +82,6 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
