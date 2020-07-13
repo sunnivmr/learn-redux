37908a439154db2747e2130876f83b6c8f45d216
sunnivmr <sunnivmr@stud.ntnu.no>
2020-07-13 16:44:07 +0200

    Created a store and ran three dispatch-actions on it

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
+// Action
 function buyCake() {
   return {
     type: BUY_CAKE,
@@ -7,10 +11,13 @@ function buyCake() {
   };
 }
 
+// Initial state
 const initialState = {
   numOfCakes: 10,
 };
 
+// Reducer
+// Controls how transitions happen
 // If action is BUY_CAKE, the number of cakes decreases by 1
 const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -24,3 +31,17 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// Creating store
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+
+// Adds listener to store
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
