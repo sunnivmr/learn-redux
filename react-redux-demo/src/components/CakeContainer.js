ba7450734bbf3ccdb19bf2a5afa590665e45c8ef
sunnivmr <sunnivmr@stud.ntnu.no>
2020-07-14 13:07:04 +0200

    #2 Removed header-content and replaced it with CakeContainer

--- react-redux-demo/src/components/CakeContainer.js
+++ react-redux-demo/src/components/CakeContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function CakeContainer() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Number of cakes</h2>
+      <button>Buy cake</button>
+    </div>
+  );
 }
 
 export default CakeContainer;
