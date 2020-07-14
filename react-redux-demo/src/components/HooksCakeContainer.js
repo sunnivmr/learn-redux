55894ccd9717ee05dca58ff23004a633f19e12d3
sunnivmr <sunnivmr@stud.ntnu.no>
2020-07-14 14:50:49 +0200

    #7 Using state and actions through yseSelector and useDispatch using React Hooks

--- react-redux-demo/src/components/HooksCakeContainer.js
+++ react-redux-demo/src/components/HooksCakeContainer.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake } from "../redux";
 
 function HooksCakeContainer() {
+  // useSelector to access state
   const numOfCakes = useSelector((state) => state.numOfCakes);
+
+  // useDispatch to dispatch actions
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h2>Number of cakes: {numOfCakes}</h2>
-      <button>Buy cake</button>
+      <button onClick={() => dispatch(buyCake())}>Buy cake</button>
     </div>
   );
 }
