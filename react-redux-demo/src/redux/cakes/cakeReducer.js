4c4268ffcda558b8c52f093cdb0b78e5518e14c8
sunnivmr <sunnivmr@stud.ntnu.no>
2020-07-14 13:24:44 +0200

    #4 Created cakeReducer

---
+++ react-redux-demo/src/redux/cakes/cakeReducer.js
@@ -0,0 +1,22 @@
+const { BUY_CAKE } = require("./cakeTypes");
+import { BUY_CAKE } from "./cakeTypes";
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+// Reducer
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state, // Makes a copy of the object
+        numOfCakes: state.numOfCakes - 1, // Changes only the chosen parameter
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default cakeReducer;
